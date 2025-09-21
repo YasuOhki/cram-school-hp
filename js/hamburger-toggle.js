0a426cd7d8c5af02899e647a3f1200bb8bb52dbc
太木 康裕 <yasu.ohki.218@gmail.com>
2025-09-21 22:45:19 +0900

    トップページのデザインを微調整, ハンバーガーメニュー押下時にクラスを追加する機能をjQueryに実装

--- js/hamburger-toggle.js
+++ js/hamburger-toggle.js
@@ -1,16 +1,19 @@
 // ハンバーガーメニューのアイコン切り替え機能（jQuery）
 $(document).ready(function() {
     $('.hamburger').on('click', function() {
+        const $button = $(this);
         const $icon = $('.hamburger__icon');
         const currentSrc = $icon.attr('src');
         
         // hamburger-menu.svgが表示されている場合はclose.svgに変更
         if (currentSrc.includes('hamburger-menu.svg')) {
             $icon.attr('src', 'image/close.svg').attr('alt', 'メニューを閉じる');
+            $button.addClass('hamburger--active');
         } 
         // close.svgが表示されている場合はhamburger-menu.svgに変更
         else if (currentSrc.includes('close.svg')) {
             $icon.attr('src', 'image/hamburger-menu.svg').attr('alt', 'メニュー');
+            $button.removeClass('hamburger--active');
         }
     });
 });
