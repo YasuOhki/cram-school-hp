9a6c3afd9a6e9c0cdf786ef05fd06ba4c32243e3
太木 康裕 <yasu.ohki.218@gmail.com>
2025-09-23 00:34:14 +0900

    ヘッダを固定表示に変更, mainVisualの画像を配置してswiperを導入(実装途中)

--- js/hamburger-toggle.js
+++ js/hamburger-toggle.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
         
         // hamburger-menu.svgが表示されている場合はclose.svgに変更
         if (currentSrc.includes('hamburger-menu.svg')) {
-            $icon.attr('src', 'image/close.svg').attr('alt', 'メニューを閉じる');
+            $icon.attr('src', 'image/icon/close.svg').attr('alt', 'メニューを閉じる');
             $button.addClass('hamburger--active');
         } 
         // close.svgが表示されている場合はhamburger-menu.svgに変更
         else if (currentSrc.includes('close.svg')) {
-            $icon.attr('src', 'image/hamburger-menu.svg').attr('alt', 'メニュー');
+            $icon.attr('src', 'image/icon/hamburger-menu.svg').attr('alt', 'メニュー');
             $button.removeClass('hamburger--active');
         }
     });
